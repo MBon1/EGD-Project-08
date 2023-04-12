00cfa56d11b4fc083cfa32345c55009060de319e
Matthew Bonnecaze <bonnecaze.matthew@gmail.com>
2023-04-12 09:50:49 -0400

    Canvas Painter

--- EGD-Project-08-Paradise/Assets/Scripts/CanvasPainter.cs
+++ EGD-Project-08-Paradise/Assets/Scripts/CanvasPainter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,11 +12,20 @@ public class CanvasPainter : TileMapPainter
     TileBase brush;
 
     public Vector2Int canvasBounds = new Vector2Int(16, 16);
+    public int textureScaleAmount = 1;
+
+    public Sprite Drawing { get; private set; }
 
     private void Start()
     {
         brush = tiles[0];
         DisplayBrushColor();
+
+
+        /*texture = new Texture2D(16, 16);
+        // Get the SpriteRenderer component and set the sprite to be the drawing texture
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));*/
     }
 
     void Update()
@@ -43,7 +53,9 @@ public class CanvasPainter : TileMapPainter
     {
         Vector3Int cellPos = tilemap.WorldToCell(mouseWorldPos);
 
-        if (Mathf.Abs(cellPos.x) < canvasBounds.x && Mathf.Abs(cellPos.y) < canvasBounds.y && !tilemap.HasTile(cellPos)) // If there is already a tile in this cell, remove it
+        Debug.Log(cellPos);
+
+        if (Mathf.Abs(cellPos.x) < canvasBounds.x && Mathf.Abs(cellPos.y) < canvasBounds.y) // If brush is in bounds
         {
             tilemap.SetTile(cellPos, brush);
         }
@@ -108,6 +120,92 @@ public class CanvasPainter : TileMapPainter
             }
         }
 
+        drawing();
+        
         return score / tileCount;
+    }
+
+    private void drawing()
+    {
+        // Create a new texture
+        Texture2D texture = new Texture2D(16, 16);
+
+        // Set the pixels to a solid red color
+        Color[] pixels = new Color[16 * 16];
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            pixels[i] = Color.clear;
+        }
+
+        Vector3Int origin = new Vector3Int(-canvasBounds.x, -canvasBounds.y, 0);
+        for (int i = 0; i < canvasBounds.x * 2; i++)
+        {
+            for (int j = 0; j < canvasBounds.y * 2; j++)
+            {
+                Vector3Int cellPos = origin;
+                cellPos.x += i;
+                cellPos.y += j;
+
+                TileBase drawnTile = tilemap.GetTile<TileBase>(cellPos);
+
+                Color color = Color.clear;
+
+                if (drawnTile != null)
+                {
+                    int index = Array.IndexOf(tiles, drawnTile);
+                    if (index >= 0)
+                        color = paletteColors[index];
+                }
+                pixels[j * 16 + i] = color;
+            }
+        }
+
+
+        texture.SetPixels(pixels);
+        texture.Apply();
+
+        Texture2D scaledTexture = ScaleTexture(texture, textureScaleAmount);
+
+        // Create a new sprite from the texture
+        Drawing = Sprite.Create(scaledTexture, new Rect(0, 0, scaledTexture.width, scaledTexture.height), Vector2.zero);
+
+        // Assign the sprite to a sprite renderer component
+        GetComponent<SpriteRenderer>().sprite = Drawing;
+    }
+
+    private Texture2D ScaleTexture(Texture2D texture, int scaleFactor)
+    {
+        int targetX = texture.width * scaleFactor;
+        int targetY = texture.height * scaleFactor;
+
+        RenderTexture rt = new RenderTexture(targetX, targetY, 24);
+        RenderTexture.active = rt;
+        Graphics.Blit(texture, rt);
+        Texture2D result = new Texture2D(targetX, targetY);
+        result.ReadPixels(new Rect(0, 0, targetX, targetY), 0, 0);
+        result.Apply();
+        return result;
+
+        /*// Calculate the new dimensions for the scaled texture
+        int newWidth = texture.width * scaleFactor;
+        int newHeight = texture.height * scaleFactor;
+
+        // Create a new texture with the desired dimensions
+        Texture2D scaledTexture = new Texture2D(newWidth, newHeight);
+
+        for (int i = 0; i < newWidth; i++)
+        {
+            for (int j = 0; j < newHeight; j++)
+            {
+                int x = Mathf.FloorToInt(i / (float)scaleFactor);
+                int y = Mathf.FloorToInt(j / (float)scaleFactor);
+                Color c = texture.GetPixel(x, y);
+                
+                scaledTexture.SetPixel(i, j, c);
+            }
+        }
+        texture.Apply();
+
+        return scaledTexture;*/
     }
 }
